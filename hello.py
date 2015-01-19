26f5a3a4019725d72f4e0f1a1a9ace59e2c0f831
Jason Smith <mookierah@gmail.com>
2015-01-19 12:54:42 +0100

    Initializing my first git repository.

---
+++ hello.py
@@ -0,0 +1,20 @@
+"""
+Some various ways of saying Hello World in Python
+"""
+
+# Define a Function
+def hello():
+    """
+    Print out Hello World in a function.
+    """
+    print("Hello World in a function.");
+    
+# Call a function that prints out Hello World
+hello()
+
+# Print out Hello World
+print("Just saying Hello World")
+
+# Assign the string Hello World to a variable and print it out
+hwStr = "Hello Word in a variable"
+print(hwStr)
